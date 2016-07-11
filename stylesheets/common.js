34ab89cca11eb626c28a7c05755b91f0c786f04d
bisheng0427 <qq120114531@gmail.com>
2016-07-11 11:31:34 +0800

    fix

--- stylesheets/common.js
+++ stylesheets/common.js
@@ -84,6 +84,16 @@ $(document).ready(function() {
 				}
 				
 			});
+
+			$("#name").click(function(e) {
+				e.preventDefault();
+			});
+			$("#phone").click(function(e) {
+				e.preventDefault();
+			});
+			$("#IDCode").click(function(e) {
+				e.preventDefault();
+			});
 			
 			function codeGen () {
 				var Num = ""; 
